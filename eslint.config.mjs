7f1306394562e77bd88a765dc062c5dab6bf4282
PrashanthPushpanathan <147142293+PrashanthPushpanathan@users.noreply.github.com>
2026-01-07 16:26:27 +0100

    Update ESLint rules and fix import quotes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,33 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 export default [
- js.configs.recommended,
- ...tseslint.configs.recommended,
- pluginReact.configs.flat.recommended,
- {
-   files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-   languageOptions: {
-     globals: {
-       ...globals.browser,
-       ...globals.node
-     }
-   },
-   settings: {
-     react: {
-       version: "detect"  // Automatisch erkennen
-     }
-   },
-   rules: {
-     "react/no-unescaped-entities": "off",
-     "@typescript-eslint/no-require-imports": "off",
-     "no-irregular-whitespace": "off",
-     "@typescript-eslint/no-unused-vars": "off",
-     "@typescript-eslint/no-explicit-any": "off",
-     "react/react-in-jsx-scope": "off",
-     "react/prop-types": "off",
-     "no-unused-vars": "off",
-   }
- },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatisch erkennen
+      },
+    },
+    rules: {
+      'react/no-unescaped-entities': 'error',
+      '@typescript-eslint/no-require-imports': 'error',
+      'no-irregular-whitespace': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      'react/react-in-jsx-scope': 'error',
+      'react/prop-types': 'error',
+      'no-unused-vars': 'error',
+    },
+  },
 ];
